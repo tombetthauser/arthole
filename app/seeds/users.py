4e314a09c51d46f9881c256d615c06b22dc4b261
tombetthauser <tombetthauser@gmail.com>
2021-05-27 00:51:41 -0700

    Get user seed working

--- app/seeds/users.py
+++ app/seeds/users.py
@@ -4,8 +4,19 @@ from app.models import db, User
 # Adds a demo user, you can add other users here if you want
 def seed_users():
 
-    demo = User(username='Demo', email='demo@aa.io',
-                password='password')
+    demo = User(
+        username='codyharris',
+        email='cody@mail.com',
+        password='password',
+        display_name='Cody X. Harris',
+        birth_year=1990,
+        location='Seattle, CA',
+        bfa='UCSC, BS Computer Science 2013',
+        mfa=None,
+        link_text='my website',
+        link_url='http://www.y9k.xyz',
+        user_type='fan'
+        )
 
     db.session.add(demo)
 
@@ -18,3 +29,40 @@ def seed_users():
 def undo_users():
     db.session.execute('TRUNCATE users;')
     db.session.commit()
+
+
+
+
+
+
+
+
+
+# from werkzeug.security import generate_password_hash
+# from app.models import db, User
+
+# # Adds a demo user, you can add other users here if you want
+# def seed_users():
+#     demo = User(username='Demo', email='demo@aa.io', password='password')
+#         # username='codyharris',
+#         # email='cody@mail.com',
+#         # password='password',
+#         # display_name='Cody X. Harris',
+#         # birth_year=1990,
+#         # location='Seattle, CA',
+#         # bfa='UCSC, BS Computer Science 2013',
+#         # mfa=None,
+#         # link_text='my website',
+#         # link_url='http://www.y9k.xyz',
+#         # user_type='fan'
+
+#     db.session.add(demo)
+#     db.session.commit()
+
+# # Uses a raw SQL query to TRUNCATE the users table.
+# # SQLAlchemy doesn't have a built in function to do this
+# # TRUNCATE Removes all the data from the table, and resets
+# # the auto incrementing primary key
+# def undo_artworks():
+#     db.session.execute('TRUNCATE artworks;')
+#     db.session.commit()
