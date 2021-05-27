d59a1fa6d7f3a9ca3808e9943ba0ac0c79fb165d
tombetthauser <tombetthauser@gmail.com>
2021-05-27 09:23:03 -0700

    Fix to_dict problem in user model

--- app/seeds/users.py
+++ app/seeds/users.py
@@ -4,7 +4,7 @@ from app.models import db, User
 # Adds a demo user, you can add other users here if you want
 def seed_users():
 
-    demo = User(
+    demo_1 = User(
         username='codyharris',
         email='cody@mail.com',
         password='password',
@@ -16,9 +16,39 @@ def seed_users():
         link_text='my website',
         link_url='http://www.y9k.xyz',
         user_type='fan'
-        )
+    )
 
-    db.session.add(demo)
+    demo_2 = User(
+        username='erinkaczkowski',
+        email='erin@mail.com',
+        password='password',
+        display_name='Erin Kaczkowski',
+        birth_year=1984,
+        location='Sacramento, CA',
+        bfa='University of Wisconsin Madison, 2009',
+        mfa='Yale University School of Art, 2011',
+        link_text='postfolio website',
+        link_url='http://www.erinkaczkowskistudio.com',
+        user_type='artist'
+    )
+
+    demo_3 = User(
+        username='tombetthauser',
+        email='tom@mail.com',
+        password='password',
+        display_name='Tom Betthauser',
+        birth_year=1987,
+        location='Sacramento, CA',
+        bfa='San Francisco Art Institute, 2010',
+        mfa='Yale University School of Art, 2012',
+        link_text='github',
+        link_url='http://www.tombetthauser.com',
+        user_type='artist'
+    )
+
+    db.session.add(demo_1)
+    db.session.add(demo_2)
+    db.session.add(demo_3)
 
     db.session.commit()
 
